9bb5f162c1bf77068e9d7f19d4e16eb018446d6e
MattE55 <89932886+MattE55@users.noreply.github.com>
2021-11-15 20:46:20 -0800

    Big Update
    Added Genres to movies
    Added director info
    Find movie data by title
    Find info about director by name
    Delete movie by title (not working)
    New user registration
    update username
    deregister (not working)

--- index.js
+++ index.js
@@ -22,60 +22,109 @@ app.use(morgan('common'));
 
 app.use(express.static('public'));
 
-let movies = [
+const movies = [
   {
     num: 1,
     title: 'Knives Out',
+    genre: 'Mystery',
     director: 'Rian Johnson'
   },
   {
     num: 2,
     title: 'Reservoir Dogs',
+    genre: 'Drama',
     director: 'Quentin Tarantino'
   },
   {
     num: 3,
     title: 'Shutter Island',
+    genre: 'Thriller',
     director: 'Martin Scorsese'
   },
   {
     num: 4,
     title: 'Kung Fu Panda',
+    genre: 'Animation',
     director: 'Mark Osborne'
   },
   {
     num: 5,
     title: 'Dolemite is my Name',
+    genre: 'Comedy',
     director: 'Craig Brewer'
   },
   {
     num: 6,
     title: 'The Other Guys',
+    genre: 'Comedy',
     director: 'Adam McKay'
   },
   {
     num: 7,
     title: 'Zodiac',
+    genre: 'Mystery',
     director: 'David Fincher'
   },
   {
     num: 8,
     title: 'Polar',
+    genre: 'Drama',
     director: 'Jonas Akerlund'
   },
   {
     num: 9,
     title: 'The Big Lebowski',
+    genre: 'Comedy',
     director: 'The Coen Brothers'
   },
   {
     num: 10,
     title: 'The Ballad of Buster Scruggs',
+    genre: 'Drama',
     director: 'The Coen Brothers'
   }
 ];
 
-let users = [
+const directors = [
+  {
+    name: 'Rian Johnson',
+    birthyear: '1973'
+  },
+  {
+    name: 'Quentin Tarantino',
+    birthyear: '1963'
+  },
+  {
+    name: 'Martin Scorsese',
+    birthyear: '1942'
+  },
+  {
+    name: 'Mark Osborne',
+    birthyear: '1970'
+  },
+  {
+    name: 'Craig Brewer',
+    birthyear: '1971'
+  },
+  {
+    name: 'Adam McKay',
+    birthyear: '1968'
+  },
+  {
+    name: 'David Fincher',
+    birthyear: '1962'
+  },
+  {
+    name: 'Jonas Akerlund',
+    birthyear: '1965'
+  },
+  {
+    name: 'The Coen Brothers',
+    birthyear: '1954 and 1957'
+  }
+]
+
+const users = [
   {
     id: 1,
     username: 'John Smith'
@@ -109,17 +158,43 @@ app.get('/users', (req, res) => {
   res.json(users);
 });
 
-//Returns data about a genre by title of movie
+//Return Data about movie by title
+
 app.get('/movies/:title', (req, res) => {
-  res.json(movies.find((movie) =>
-    { return movie.genre === req.params.genre }));
+const movie = movies.find((movies) =>
+    { return movies.title === req.params.title });
+ if (movie) {
+
+    res.status(201).send(movie);
+  } else {
+    res.status(404).send('Movie ' + req.params.name + ' not found.');
+  }
+});
+
+
+//Returns data about a genre by title of movie
+app.get('/movies/:title/genre', (req, res) => {
+const movie = movies.find((movies) =>
+    { return movies.genre === req.params.title });
+ if (movie) {
+    res.status(201).send(movies.genre);
+  } else {
+    res.status(404).send('Movie ' + req.params.title + ' not found.');
+  }
 });
 
 
 //Return Data about director by name
-app.get('/movies/:director', (req, res) => {
-  res.json(movies.find((movies) =>
-    { return movies.director === req.params.director }));
+
+app.get('/directors/:name', (req, res) => {
+const director = directors.find((directors) =>
+    { return directors.name === req.params.name });
+ if (director) {
+
+    res.status(201).send(director);
+  } else {
+    res.status(404).send('Director ' + req.params.name + ' not found.');
+  }
 });
 
 
@@ -143,11 +218,55 @@ app.delete('/movies/:title', (req, res) => {
   let movie = movies.find((movie) => {
     return movie.title === req.params.title
   });
+
   if (movie) {
     movies = movies.filter((obj) => { return obj.title !== req.params.title });
     res.status(201).send('Movie ' + req.params.title + ' was deleted.');
+  } else {
+    res.status(404).send('Movie ' + req.params.title + ' not found.');
+  }
+});
+
+//Allow new users to register
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.username) {
+    const message = 'missing username in request body';
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+// Update username
+app.put('/users/:username', (req, res) => {
+  let user = users.find((user) => { return user.username === req.params.username });
+
+  if (user) {
+    user.username[req.params.username] = parseInt(req.params.username);
+    res.status(201).send('Username ' + req.params.username + ' was updated ');
+  } else {
+    res.status(404).send('Account with username ' + req.params.username + ' was not found.');
+  }
+});
+
+// Existing users to deregister
+app.delete('/users/:username', (req, res) => {
+  const user = users.find((user) => {
+    return user.username === req.params.username
+  });
+  if (user) {
+    users = users.filter((obj) => { return obj.username !== req.params.username });
+    res.status(201).send('User ' + req.params.username + ' was deleted.');
+  } else {
+    res.status(404).send('Username ' + req.params.username + ' not found.');
   }
 })
+
+
 
 
 
